4b61030f15fa6aa791ab95bf7010c4cef3091cc2
Murphy-Joe <ibidnsell@yahoo.com>
2021-11-15 11:16:02 -0500

    init commit

---
+++ StatusApi/Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatusApi.Controllers;
+public class StatusController : ControllerBase
+{
+    // http get localhost:5000/status
+    [HttpGet("/status")]
+    public ActionResult GetTheStatus()
+    {
+        var resp = new StatusResponse
+        {
+            Message = "The server is great thanks",
+            LastChecked = DateTime.Now
+        };
+        return Ok(resp);
+    }
+}
+
+public class StatusResponse
+{
+    public string Message { get; set; }
+    public DateTime LastChecked { get; set; }  
+}
